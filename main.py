517d6c0201634c979a35fa30583da94f40152a9b
Astra Berry <astraberry82@gmail.com>
2022-06-10 22:21:56 +0000

    Added comment and f string

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 print("Welcome to Band Name Generator ")
 city_name = input("What city did you grow up in? \n")
 pet_name = input("What is your pet's name? \n")
-print("Here is your band name: " + city_name + " " + pet_name)
+#printing both inputs user adds and adding them together
+print(f"Here is your band name:  {city_name} {pet_name}")
